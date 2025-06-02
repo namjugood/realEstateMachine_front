78a491a2dc34a99e915eee456bd41377a7682e48
hy7721 <azipark7721@gmail.com>
2025-06-02 14:39:35 +0900

    tailwind CSS 설치

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import './assets/css/global.css'
+import './assets/css/App.css'
 import { router } from './router'
 
 createRoot(document.getElementById('root')!).render(
